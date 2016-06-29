159cb8a8835763caca75633890520212300438b2
Karl Cronburg <kcronbur@redhat.com>
2016-06-29 18:07:54 -0400

    np import cleaner, debugging asserts, more descriptive comments, fixed bug where we were ignoring samples which started before and ended after the interval (could have affected small intervals / large latency samples), ctx.interval, default to 10 second intervals

--- fio-latlog-stats.py
+++ fio-latlog-stats.py
@@ -1,22 +1,43 @@
 #!/usr/bin/env python2.7
 import sys
 import numpy as np
-from numpy import genfromtxt, lexsort, around, where, logical_and, logical_or
-land = logical_and
-lor  = logical_or
+from numpy import genfromtxt, lexsort, around, where
+land, lor, lnot = np.logical_and, np.logical_or, np.logical_not
 from itertools import islice
 import argparse
+import os
+
+debug = not (os.getenv("DEBUG") is None)
 
 def weighted_percentile(percs, vs, ws):
-    """ Use linear interpolation to calculate the weighted percentile """
+    """ Use linear interpolation to calculate the weighted percentile.
+        
+        Value and weight arrays are first sorted by value. The cumulative
+        distribution function (cdf) is then computed, after which np.interp
+        finds the two values closest to our desired weighted percentile(s)
+        and linearly interpolates them.
+        
+        percs  :: List of percentiles we want to calculate
+        vs     :: Array of values we are computing the percentile of
+        ws     :: Array of weights for our corresponding values
+        return :: Array of percentiles
+    """
     idx = np.argsort(vs)
     vs, ws = vs[idx], ws[idx] # weights and values sorted by value
-    cdf = 100 * ws.cumsum() / ws.sum() # cumulative distribution function
+    cdf = 100 * ws.cumsum() / ws.sum()
     return np.interp(percs, cdf, vs) # linear interpolation
 
 def weights(start_ts, end_ts, start, end):
     """ Calculate weights based on fraction of sample falling in the
-        given interval [start,end] """
+        given interval [start,end]. Weights computed using vector / array
+        computation instead of for-loops.
+    
+        start_ts :: Array of start times for a set of samples
+        end_ts   :: Array of end times for a set of samples
+        start    :: int
+        end      :: int
+        return   :: Array of weights
+    """
     sbounds = np.maximum(start_ts, start).astype(float)
     ebounds = np.minimum(end_ts,   end).astype(float)
     return (ebounds - sbounds) / (end_ts - start_ts)
@@ -24,38 +45,41 @@ def weights(start_ts, end_ts, start, end):
 columns = ["end-time", "samples", "min", "avg", "median", "90%", "95%", "99%", "max"]
 percs   = [50, 90, 95, 99]
 print(', '.join(columns))
-INTERVAL = 10000
-
 def process_interval(samples, start, end):
+    """ Determine which of the given samples occur during the given interval,
+        then compute and print the desired statistics - min, avg, percentiles, max.
+
+        samples :: Array / matrix of samples (as seen in fio log files)
+        start   :: int
+        end     :: int
+    """
     
     times,clats = samples[:,0], samples[:,1]
-    start_times = times - (clats / 1000.0)
+    start_times = times - (clats / 1000.0)   # convert end time array to start times
     # Sort by start time:    
     idx = lexsort((start_times,))
     samples = samples[idx]
     times,clats = samples[:,0], samples[:,1]
     start_times = start_times[idx]
 
-    # Determine which samples are in the current window:
-    idx = where(lor ( land(start_times >= start, start_times <  end)
-                    , land(times       >  start, times       <= end)))
+    # Determine which samples occured during the current interval [start,end]:
+    idx = where(lnot(lor(start_times >= end, times <= start)))
     ss = samples[idx]
+    start_ts = start_times[idx]
+    end_ts, vs = ss[:,0], ss[:,1]
+    
     if len(ss) > 0:
-        
-        start_ts = start_times[idx]
-        end_ts, vs = ss[:,0], ss[:,1]
-
-        # Compute weights and subsequently compute the weighted
-        # percentiles we are interested in:
         ws = weights(start_ts, end_ts, start, end)
+        if debug: assert(np.all(ws > 0))
         ps = weighted_percentile(percs, vs, ws)
 
-        # Output formatting same as current fiologparser:
+        # Output formatting same as '-A' option of fiologparser:
         row = [end, len(ss), np.min(vs), np.average(vs)] + list(ps) + [np.max(vs)]
         fmt = "%d, %d, " + ', '.join(["%.4f"] * 7)
         print (fmt % tuple(row))
 
 def read_next(fp, sz):
+    """ Helper to get rid of 'empty file' warnings """
     with np.warnings.catch_warnings():
         np.warnings.simplefilter("ignore")
         return genfromtxt(islice(fp, sz), dtype=int, delimiter=',')
@@ -63,13 +87,13 @@ def read_next(fp, sz):
 def main(ctx):
     with open(ctx.filename, 'r') as fp:
         start = 0
-        end = INTERVAL
+        end = ctx.interval
         arr = read_next(fp, ctx.buff_size)
         more_data = True
         while more_data or len(arr) > 0:
 
             # Read up to 5 minutes of data from end of current interval.
-            while len(arr) == 0 or arr[-1][0] < 300 * 1000 + end:
+            while len(arr) == 0 or arr[-1][0] < ctx.max_latency * 1000 + end:
                 new_arr = read_next(fp, ctx.buff_size)
                 if new_arr.shape[0] < ctx.buff_size:
                     more_data = False
@@ -84,15 +108,15 @@ def main(ctx):
             idx = where(arr[:,0] > end)
             arr = arr[idx]
 
-            start += INTERVAL
-            end = start + INTERVAL
+            start += ctx.interval
+            end = start + ctx.interval
 
 if __name__ == '__main__':
     p = argparse.ArgumentParser()
     arg = p.add_argument
     arg('-f', '--filename', required=True, help='filename of latency log file')
     arg('--max_latency', default=300, type=float, help='number of seconds of data to process at a time')
-    arg('-i', '--interval', default=1000, type=int, help='interval width (ms)')
+    arg('-i', '--interval', default=10000, type=int, help='interval width (ms)')
     arg('--buff_size', default=10000, type=int, help='number of samples to buffer into numpy at a time')
     main(p.parse_args())
 
