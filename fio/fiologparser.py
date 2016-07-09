some error checking, matching argument spec of fiologparser, latency vs bandwidth start time calculation, numpy array shape bug, still losing (up to #files?) lines at end of file(s)?, added source file info in numpy array, fixed bug with losing big chunks of data

fio/fiologparser.py
@@ -9,6 +9,7 @@ import os
 import pandas
 
 debug = not (os.getenv("DEBUG") is None)
+err = sys.stderr.write
 
 def weighted_percentile(percs, vs, ws):
     """ Use linear interpolation to calculate the weighted percentile.
@@ -33,6 +34,9 @@ def weights(start_ts, end_ts, start, end):
         given interval [start,end]. Weights computed using vector / array
         computation instead of for-loops.
     
+        Note that samples with zero time length are effectively ignored
+        (we set their weight to zero). TODO: print warning always?
+
         start_ts :: Array of start times for a set of samples
         end_ts   :: Array of end times for a set of samples
         start    :: int
@@ -41,11 +45,60 @@ def weights(start_ts, end_ts, start, end):
     """
     sbounds = np.maximum(start_ts, start).astype(float)
     ebounds = np.minimum(end_ts,   end).astype(float)
-    return (ebounds - sbounds) / (end_ts - start_ts)
+    ws = (ebounds - sbounds) / (end_ts - start_ts)
+    if debug and np.any(np.isnan(ws)):
+      err("WARNING: zero-length sample(s) detected. Possible culprits:\n")
+      err("  1) Using -bw when you should be using -lat.\n")
+      err("  2) Log file has bad or corrupt time values.\n")
+    ws[np.where(np.isnan(ws))] = 0.0;
+    return ws
 
 columns = ["end-time", "samples", "min", "avg", "median", "90%", "95%", "99%", "max"]
 percs   = [50, 90, 95, 99]
 print(', '.join(columns))
+
+def fmt_float_list(ctx, num=1):
+  """ Return a comma separated list of float formatters to the required number
+      of decimal places. For instance:
+
+        fmt_float_list(ctx.decimals=4, num=3) == "%.4f, %.4f, %.4f"
+  """
+  return ', '.join(["%%.%df" % ctx.decimals] * num)
+
+def print_sums(ctx, vs, ws, ss, end, divisor=1.0):
+    fmt = "%d, " + fmt_float_list(ctx, 1)
+    print (fmt % (end, np.sum(vs * ws) / divisor / ctx.divisor)) 
+
+def print_averages(ctx, vs, ws, ss, end):
+    print_sums(ctx, vs, ws, ss, end, divisor=float(len(vs)))
+
+def print_full(ctx, vs, ws, ss, end):
+    fmt = "%d, " + fmt_float_list(ctx, len(ctx.FILE))
+
+    # List of lists of where the last column in the samples is all the same
+    # (corresponding to which file the input came from)
+    idxs = [where(ss[:,-1] == i) for i in range(len(ctx.FILE))]
+    
+    # Each column in this row corresponds to the weighted sum of samples
+    # falling in the current interval in a particular file:
+    row = [np.sum(vs[idxs[i]] * ws[idxs[i]]) for i in range(len(ctx.FILE))]
+    
+    print (fmt % tuple([end] + row))
+
+def print_all_stats(ctx, vs, ws, ss, end):
+    ps = weighted_percentile(percs, vs, ws)
+
+    # Output formatting same as '-A' option of fiologparser:
+    values = [np.min(vs), np.average(vs)] + list(ps) + [np.max(vs)]
+    row = [end, len(ss)] + map(lambda x: float(x) / ctx.divisor, values)
+    fmt = "%d, %d, " + fmt_float_list(ctx, 7)
+    print (fmt % tuple(row))
+
+# TODO: not sure what the default is doing yet.
+def print_default(ctx, vs, ws, ss, end):
+    pass
+    #print (fmt_float_list(ctx, 1) % (np.sum(vs * ws) / ctx.divisor,))
+
 def process_interval(ctx, samples, start, end):
     """ Determine which of the given samples occur during the given interval,
         then compute and print the desired statistics - min, avg, percentiles, max.
@@ -56,7 +109,14 @@ def process_interval(ctx, samples, start, end):
     """
     
     times,clats = samples[:,0], samples[:,1]
-    start_times = times - (clats / 1000.0)   # convert end time array to start times
+   
+    if ctx.latency:
+      start_times = times - (clats / 1000.0) # convert end time array to start times
+    elif ctx.bandwidth:
+      start_times = np.delete(np.insert(times, 0, 0), times.size)
+    else:
+      raise Exception("Please specify either --bandwidth or --latency.")
+
     # Sort by start time:    
     idx = lexsort((start_times,))
     samples = samples[idx]
@@ -71,19 +131,18 @@ def process_interval(ctx, samples, start, end):
     
     if len(ss) > 0:
         ws = weights(start_ts, end_ts, start, end)
-        if debug: assert(np.all(ws > 0))
-        ps = weighted_percentile(percs, vs, ws)
-
-        # Output formatting same as '-A' option of fiologparser:
-        row = [end, len(ss), np.min(vs), np.average(vs)] + list(ps) + [np.max(vs)]
-        fmt = "%d, %d, " + ', '.join(["%%.%df" % ctx.decimals] * 7)
-        print (fmt % tuple(row))
+        
+        if ctx.sum:         print_sums(ctx, vs, ws, ss, end)
+        elif ctx.average:   print_averages(ctx, vs, ws, ss, end)
+        elif ctx.full:      print_full(ctx, vs, ws, ss, end)
+        elif ctx.allstats:  print_all_stats(ctx, vs, ws, ss, end)
+        else:               print_default(ctx, vs, ws, ss, end)
 
 def read_csv(fp, sz):
     try:
       return pandas.read_csv(Reader(islice(fp, sz)), dtype=int, header=None).values
     except ValueError:
-      return np.empty(0)
+      return np.empty((0,5))
 
 def read_next(fp, sz):
     """ Helper to get rid of 'empty file' warnings """
@@ -95,6 +154,8 @@ def read_next(fp, sz):
             return np.array([data]) # Single-line files are dumb.
         return data
 
+# TODO: seem to be losing data points at the end of the file - make sure the
+# lines dictionary here is being fully flushed by the generator...
 def fio_generator(fps):
     """ Create a generator for reading multiple fio files in end-time order """
     lines = {fp: fp.next() for fp in fps}
@@ -102,7 +163,7 @@ def fio_generator(fps):
     while True:
         # Get fp with minimum value in the first column (fio log end-time value)
         fp = min(lines, key=lambda k: int(lines.get(k).split(',')[0]))
-        yield lines[fp]
+        yield (lines[fp].rstrip() + ", " + str(fps.index(fp)) + '\n')
         lines[fp] = fp.next() # read a new line into our dictionary
 
 class Reader(object):
@@ -118,7 +179,7 @@ class Reader(object):
 #df = pd.concat([chunk[chunk['field'] > constant] for chunk in iter_csv])
 
 def main(ctx):
-    fps = [open(f, 'r') for f in ctx.files]
+    fps = [open(f, 'r') for f in ctx.FILE]
     fp = fio_generator(fps)
     
     try:
@@ -133,6 +194,7 @@ def main(ctx):
                 new_arr = read_next(fp, ctx.buff_size)
                 if new_arr.shape[0] < ctx.buff_size:
                     more_data = False
+                    arr = np.append(arr, new_arr, axis=0)
                     break
                 arr = np.append(arr, new_arr, axis=0)
 
@@ -146,17 +208,24 @@ def main(ctx):
 
             start += ctx.interval
             end = start + ctx.interval
+        
     finally:
         map(lambda f: f.close(), fps)
 
 if __name__ == '__main__':
     p = argparse.ArgumentParser()
     arg = p.add_argument
-    arg('-f', '--files', required=True, help='space separated list of latency log filenames', nargs='+')
     arg('--max_latency', default=300, type=float, help='number of seconds of data to process at a time')
     arg('-i', '--interval', default=10000, type=int, help='interval width (ms)')
     arg('-d', '--divisor', required=False, type=int, default=1, help='divide the results by this value.')
-		arg('--buff_size', default=10000, type=int, help='number of samples to buffer into numpy at a time')
-    arg('-d', '--decimals', default=3, type=int, help='number of decimal places to print floats to')
+    arg('-f', '--full', dest='full', action='store_true', default=False, help='print full output.')
+    arg('-A', '--all', dest='allstats', action='store_true', default=False, help='print all stats for each interval.')
+    arg('-a', '--average', dest='average', action='store_true', default=False, help='print the average for each interval.')
+    arg('-s', '--sum', dest='sum', action='store_true', default=False, help='print the sum for each interval.') 
+    arg('--buff_size', default=10000, type=int, help='number of samples to buffer into numpy at a time')
+    arg('--decimals', default=3, type=int, help='number of decimal places to print floats to')
+    arg('-bw', '--bandwidth', dest='bandwidth', action='store_true', default=False, help='input contains bandwidth log files.')
+    arg('-lat', '--latency', dest='latency', action='store_true', default=False, help='input contains latency log files.')
+    arg("FILE", help='space separated list of latency log filenames', nargs='+')
     main(p.parse_args())
 